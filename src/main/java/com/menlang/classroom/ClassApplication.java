779265942da3d01d0fe5cb1909235a1f94efaa4d
Menglang Huo <huomenglang@gmail.com>
2025-07-02 21:17:43 +0700

    fix: unknown Gloable Exception in common-library

--- src/main/java/com/menlang/classroom/ClassApplication.java
+++ src/main/java/com/menlang/classroom/ClassApplication.java
@@ -3,7 +3,11 @@ package com.menlang.classroom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = {
+		"com.menlang.classroom",              // your microservice code
+		"com.menglang.common.library"        // your common-library code
+})
 public class ClassApplication {
 
 	public static void main(String[] args) {
